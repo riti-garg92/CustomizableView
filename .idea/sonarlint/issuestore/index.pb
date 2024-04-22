35932fb7110e2bf3ea8ddbd7264849bbcad220e2
riti-garg92 <167803970+riti-garg92@users.noreply.github.com>
2024-04-23 00:17:39 +0530

    initial

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -26,3 +26,11 @@ C
 settings.gradle.kts,7\4\749edfcc96398253e5b3416184e95c46621da850
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+q
+Aapp/src/main/java/p/ritika/customizableview/CustomizableView.java,3\4\3466132b9d72dbef71e13a79374235c1bf4b2762
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library")
+    id ("maven-publish")
 }
 
 android {
@@ -7,6 +8,7 @@ android {
 
     defaultConfig {
         minSdk = 21
+        compileSdk = 34
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -28,8 +30,21 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.riti-garg92"
+            artifactId = "my-library"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
